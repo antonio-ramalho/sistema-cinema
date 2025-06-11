a1dfb542d271f182b4234c68e23ebaaaba80d310
Felipe Postigo <Fesilvapostigo2007@gmail.com>
2025-06-11 17:27:35 -0300

    Update filmes.py

--- filmes.py
+++ filmes.py
@@ -3,5 +3,8 @@ import menu_principal
 filmes = []
 
 def cadastra_filme():
-    nome_filme = input('Digite o nome do filme')
-    pass
+    while True:
+        nome_filme = input('Digite o nome do filme: ')
+        genero = input('Digite o gênero do filme: ')
+        lancamento = input('Digite o ano de lançamento do filme: ')
+        
