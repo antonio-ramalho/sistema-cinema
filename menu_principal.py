c6e1988d48e5c10e7026e369794ab0c4e97b54d6
Antonio Ramalho <antoninho.ferreira2015@gmail.com>
2025-06-11 13:13:12 -0300

    Atualização da interface grafica

--- menu_principal.py
+++ menu_principal.py
@@ -4,34 +4,65 @@ def limpar_console():
     os.system('cls' if os.name == 'nt' else 'clear')
 
 def menu_sistema():
+    
     print("-" * 60)
     print("CINEMAX".center(60))
     print("-" * 60)
 
-while True:
+while True: # loop principal
+    menu_escolha = ["[1] Filmes em cartaz", "[2] Entrar/Cadastrar", "[3] Sair"]
     menu_sistema()
     
-    print("[1] Filmes em cartaz")
-    print("[2] Entrar/Cadastrar")
-    print("[3] Sair")
-    
+    for i in menu_escolha:
+        print(i)
+        
     resposta = input("Digite sua escolha: ")
     
-    while True:
-        if resposta == "1":
-            limpar_console()
+    limpar_console()
+    
+    while True: # loop escolha
+        
+        if resposta == "1": # primeira escolha
             menu_sistema()
             
             # Inicio do modulo
-        elif resposta == "2":
-            limpar_console()
+            
+            print("[1] Sair")
+            
+            if input("Digite sua escolha: ") == "1":
+                limpar_console()
+                break
+            else:
+                limpar_console()
+                print(" ")
+                print("****Digite um número válido!****".center(60))
+                print(" ")
+                
+        elif resposta == "2": # segunda escolha
             menu_sistema()
             
             # Inicio do modulo
-        elif resposta == "3":
-            print("-" * 60)
+            
+            print("[1] Entrar")
+            print("[2] Cadastrar-se")
+            print("[3] Sair")
+            
+            if input("Digite sua escolha: ") == "3":
+                limpar_console()
+                break
+            else:
+               limpar_console()
+               print(" ")
+               print("****Digite um número válido!****".center(60))
+               print(" ")
+            
+        elif resposta == "3": # terceira escolha
+            menu_sistema()
             print("Obrigado pela preferência, volte sempre!".center(60))
+            print("-" * 60)
             exit()
         else:
-            print("Digite um número válido!")
-        
+            print(" ")
+            print("****Digite um número válido!****".center(60))
+            print(" ")
+            break
