d5ded07dfb57ecb3e34c49029c10315b4a9c9775
Antonio Ramalho <antoninho.ferreira2015@gmail.com>
2025-06-15 19:51:00 -0300

    refatoração da interface gráfica

--- menu_principal.py
+++ menu_principal.py
@@ -1,87 +1,47 @@
-import os
+
 import filmes
-
-def limpar_console():
-    os.system('cls' if os.name == 'nt' else 'clear')
-
-def menu_sistema():
-    
-    print("-" * 60)
-    print("CINEMAX".center(60))
-    print("-" * 60)
+import utils_armazenamento as u_a
 
 while True: # loop principal
-    try:
-        menu_escolha = ["[1] Filmes em cartaz", "[2] Entrar/Cadastrar", "[3] Sair"]
-        menu_sistema()
-        
-        for i in menu_escolha:
-            print(i)
+    u_a.cabecalho_cinemax()
+    resposta = u_a.menu_sistema()
+    u_a.limpar_console()
+          
+    while True: # loop escolha
+        if resposta == "1": # primeira escolha
+                
+            filmes.mostrar_filmes(filmes.catalogo)
+                
+            print("[1] Sair")
+            print(" ")
             
-        resposta = int(input("Digite sua escolha: "))
-        
-        limpar_console()
-        
-    
-        while True: # loop escolha
-            
-            if resposta == 1: # primeira escolha
-                
-                filmes.mostrar_filmes(filmes.catalogo)
-                
-                print("[1] Sair")
-                print(" ")
-                try:
-                    if int(input("Digite sua escolha: ")) == 1:
-                        limpar_console()
-                        break
-                    else:
-                        limpar_console()
-                        print(" ")
-                        print("****Digite um número válido!****".center(60))
-                        print(" ")
-                except ValueError:
-                        limpar_console()
-                        print(" ")
-                        print("****Por favor, digite um número!****".center(60))
-                        print(" ")
-                    
-            elif resposta == 2: # segunda escolha
-                menu_sistema()
-                
-                # Inicio do modulo
-                
-                print("[1] Entrar")
-                print("[2] Cadastrar-se")
-                print("[3] Sair")
-                
-                try:
-                    if int(input("Digite sua escolha: ")) == 3:
-                        limpar_console()
-                        break
-                    else:
-                        limpar_console()
-                        print(" ")
-                        print("****Digite um número válido!****".center(60))
-                        print(" ")
-                except ValueError:
-                        limpar_console()
-                        print(" ")
-                        print("****Por favor, digite um número!****".center(60))
-                        print(" ")
-                
-            elif resposta == 3: # terceira escolha
-                menu_sistema()
-                print("Obrigado pela preferência, volte sempre!".center(60))
-                print("-" * 60)
-                exit()
-            else:
-                print(" ")
-                print("****Digite um número válido!****".center(60))
-                print(" ")
+            if int(input("Digite sua escolha: ")) == 1:
+                u_a.limpar_console()
                 break
-    except ValueError:
-        limpar_console()
-        print(" ")
-        print("****Por favor, digite um número!****".center(60))
-        print(" ")
+            else:
+                u_a.limpar_console()
+                u_a.msg_numero_valido()
+                u_a.cabecalho_cinemax()
+        elif resposta == "2": # segunda escolha
+            u_a.cabecalho_cinemax()
+                
+            # Inicio do modulo 
+                
+            print("[1] Entrar")
+            print("[2] Cadastrar-se")
+            print("[3] Sair")
+                
+            if int(input("Digite sua escolha: ")) == 3:
+                u_a.limpar_console()
+                break
+            else:
+                u_a.limpar_console()
+                u_a.msg_numero_valido()  
+        elif resposta == "3": # terceira escolha
+            u_a.cabecalho_cinemax()
+            print("Obrigado pela preferência, volte sempre!".center(60))
+            print("-" * 60)
+            exit()
+        else:
+            u_a.msg_numero_valido()
+            break
---
+++ utils_armazenamento.py
@@ -0,0 +1,26 @@
+# funções gráficas e de armazenamento usadas em todo o sistema
+
+import os
+
+def menu_sistema(): 
+    menu_escolha = ["[1] Filmes em cartaz", "[2] Entrar/Cadastrar", "[3] Sair"]
+    
+    for i in menu_escolha:
+        print(i)
+    
+    resposta = input("Digite sua escolha: ")
+    
+    return resposta
+
+def limpar_console():
+    os.system('cls' if os.name == 'nt' else 'clear')
+
+def cabecalho_cinemax():
+    print("-" * 60)
+    print("CINEMAX".center(60))
+    print("-" * 60)
+            
+def msg_numero_valido():
+    print(" ")
+    print("****Digite um número válido!****".center(60))
+    print(" ")
